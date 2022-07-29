0fec309e9fc5c7fcf6cdc8017adb8495c595fc5e
DrugCoding <lhs940308@gmail.com>
2022-07-29 17:22:33 +0900

    7월29일 금요일 알고리즘

--- 1회차/이현성/4_신용카드만들기1.py
+++ 1회차/이현성/4_신용카드만들기1.py
@@ -1,3 +1,19 @@
 import sys
 
 sys.stdin = open("_신용카드만들기1.txt")
+
+a = int(input())
+card = []
+for i in range(a):
+    num_ = input()
+    card_ = []
+    if num_[0] == '3' or num_[0] == '4' or num_[0] == '5' or num_[0] == '6' or num_[0] == '9':
+        card_.append(num_)
+        print(card_)
+        sum_ = 0
+        for i in range(a):
+            sum_ += card_.count(i)
+            if sum_ > 16:
+                card.append(card_)
+
+print(card)
